b91603f6e97e3a84c565c5d2c648260c383bd4de
keesj <kees.jongenburger@gmail.com>
2021-02-25 14:03:33 +0100

    Access

--- uart_swim/access.py
+++ uart_swim/access.py
@@ -3,8 +3,11 @@ from serial import Serial
 import string
 import time
 
-with Serial("/dev/ttyACM0",timeout=1) as dev:
-    dev.baudrate = 115200
-    for i in range(0,100):
-            dev.write("a".encode())
-            time.sleep(2)
+try:
+        with Serial("/dev/ttyACM0",timeout=1) as dev:
+            dev.baudrate = 115200
+            for i in range(0,100):
+                    dev.write("a".encode())
+                    time.sleep(2)
+except:
+    print("closing ...")
