5ba1208c05f9126e73b88802c452f6b3e2e823ee
keesj <kees.jongenburger@gmail.com>
2021-03-04 10:47:43 +0100

    container currently builds

---
+++ .devcontainer/build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+set -x
+# http://www.clifford.at/icestorm/
+sudo apt-get update
+sudo apt-get -y install build-essential clang bison flex libreadline-dev \
+                     gawk tcl-dev libffi-dev git mercurial graphviz   \
+                     xdot pkg-config python python3 libftdi-dev \
+                     qt5-default python3-dev libboost-all-dev cmake libeigen3-dev
+
+ 
+(
+git clone https://github.com/YosysHQ/icestorm.git icestorm
+cd icestorm
+make -j$(nproc)
+sudo make install
+)
+
+(
+git clone https://github.com/cseed/arachne-pnr.git arachne-pnr
+cd arachne-pnr
+make -j$(nproc)
+sudo make install
+)
+
+(
+git clone https://github.com/YosysHQ/nextpnr nextpnr
+cd nextpnr
+cmake -DARCH=ice40 -DCMAKE_INSTALL_PREFIX=/usr/local .
+make -j$(nproc)
+sudo make install
+)
+
+(
+git clone https://github.com/YosysHQ/yosys.git yosys
+cd yosys
+make -j$(nproc)
+sudo make install
+)
