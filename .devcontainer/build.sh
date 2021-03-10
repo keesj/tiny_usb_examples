80deea31e369f97eb82c1da7dac91b7073b5c6d0
keesj <kees.jongenburger@gmail.com>
2021-03-10 08:58:17 +0100

    nextpnr need a recursive clone

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -3,12 +3,14 @@ set -e
 set -x
 # http://www.clifford.at/icestorm/
 sudo apt-get update
-sudo apt-get -y install build-essential clang bison flex libreadline-dev \
+sudo  DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential clang bison flex libreadline-dev \
                      gawk tcl-dev libffi-dev git mercurial graphviz   \
                      xdot pkg-config python python3 libftdi-dev \
                      qt5-default python3-dev libboost-all-dev cmake libeigen3-dev
 
- 
+
+rm -rf icestorm arachne-pnr nextpnr yosys
+
 (
 git clone https://github.com/YosysHQ/icestorm.git icestorm
 cd icestorm
@@ -24,7 +26,7 @@ sudo make install
 )
 
 (
-git clone https://github.com/YosysHQ/nextpnr nextpnr
+git clone --recursive https://github.com/YosysHQ/nextpnr nextpnr
 cd nextpnr
 cmake -DARCH=ice40 -DCMAKE_INSTALL_PREFIX=/usr/local .
 make -j$(nproc)
@@ -37,4 +39,7 @@ cd yosys
 make -j$(nproc)
 sudo make install
 )
+
 rm -rf icestorm arachne-pnr nextpnr yosys
+
+
